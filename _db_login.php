fded18d6e53ce1796ba9dea4f846a145f383f05a
Karim Dallas-Newell <karim@devslow.tech>
2017-10-06 18:37:23 -0400

    file to be included in any file accessing db; has ini_set to show errors while testing

---
+++ _db_login.php
@@ -0,0 +1,17 @@
+<?php 
+
+	ini_set('display_errors', 1); //displays errors FOR TESTING ONLY
+
+	$dsn = 'mysql:dbname=cdg9;host=sql2.njit.edu';
+	$username = 'cdg9';
+	$password = 'x9kbkWsY';
+	$table = 'users';
+
+	try {
+		$conn = new PDO($dsn, $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo 'success';
+	} catch (PDOException $e) {
+		echo 'failed ' . $e->getMessage();
+	} 
+?>
