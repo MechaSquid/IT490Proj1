bb46874933d26b79b092431cb481a2d6996181a7
Karim Dallas-Newell <karim@devslow.tech>
2017-10-11 07:30:51 -0400

    Commented out a line

--- .db_login.php
+++ .db_login.php
@@ -1,6 +1,6 @@
 <?php 
 
-	ini_set('display_errors', 1); //displays errors FOR TESTING ONLY
+	//ini_set('display_errors', 1); //displays errors FOR TESTING ONLY
 
 	$dsn = 'mysql:dbname=cdg9;host=sql2.njit.edu';
 	$username = 'cdg9';
