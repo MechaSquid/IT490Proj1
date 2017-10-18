77117d6f0885ddc1f31356ae72adad42806ceabd
Karim Dallas-Newell <karim@devslow.tech>
2017-10-18 18:46:01 -0400

    added eventlisteners for future func

--- search/flight_search.php
+++ search/flight_search.php
@@ -13,27 +13,27 @@
 <body>
 	<form action="search_results.php" method="POST">
 		<label for="flight-number">Flight Number</label>
-		<input type="text" list="flight-numbers" id="flight-number" name="flight-number" onfocus="pullChoices(this.name)">
+		<input type="text" list="flight-numbers" id="flight-number" name="flight-number" onfocus="pullFlightChoices(this.name)">
 		<datalist id="flight-numbers"></datalist>
 		
 		<label for="tail-number">Tail Number</label>
-		<input type="text" list="tail-numbers" id="tail-number" name="tail-number" onfocus="pullChoices(this.name)">
-		<datalist id="flight-numbers"></datalist>
+		<input type="text" list="tail-numbers" id="tail-number" name="tail-number" onfocus="pullFlightChoices(this.name)">
+		<datalist id="tail-numbers"></datalist>
 		
 		<label for="origin">Origin</label>
-		<input type="text" list="origins" id="origin" name="origin" onfocus="pullChoices(this.name)">
+		<input type="text" list="origins" id="origin" name="origin" onfocus="pullFlightChoices(this.name)">
 		<datalist id="origins"></datalist>
 		
 		<label for="destination">Destination</label>
-		<input type="text" list="destinations" id="destination" name="destination" onfocus="pullChoices(this.name)">
+		<input type="text" list="destinations" id="destination" name="destination" onfocus="pullFlightChoices(this.name)">
 		<datalist id="destinations"></datalist>
 		
 		<label for="cargo">Cargo</label>
-		<input type="text" list="cargos" id="cargo" name="cargo" onfocus="pullChoices(this.name)">
+		<input type="text" list="cargos" id="cargo" name="cargo" onfocus="pullFlightChoices(this.name)">
 		<datalist id="cargos"></datalist>
 		
 		<label for="crew-id">Crew ID</label>
-		<input type="text" list="crew-ids" id="crew-id" name="crew-id" onfocus="pullChoices(this.name)">
+		<input type="text" list="crew-ids" id="crew-id" name="crew-id" onfocus="pullFlightChoices(this.name)">
 		<datalist id="crew-ids"></datalist>
 		
 		<button>Search</button>
